ca634fe63a41b0d819e6a26313b3cd5bfd77a7fd
William Meyer Baumbacher <classwba@lnxsrv10.seas.ucla.edu>
2020-02-15 11:55:45 -0800

    lab done

--- as5/tr2b.c
+++ as5/tr2b.c
@@ -5,7 +5,6 @@ in spec */
 #include <stdlib.h>
 #include <string.h>
 
-// Your implementations should report an error from and to are not the same length, or if from has duplicate bytes.
 void checkInputs(int argc, char **argv) {
   if (argc > 3) {
     fprintf(stderr, "Error: too many operands.");
@@ -15,14 +14,14 @@ void checkInputs(int argc, char **argv) {
     fprintf(stderr, "Error: too few operands.");
     exit(1);
   }
-  else if (strlen(argv[0]) != strlen(argv[1])) {
+  else if (strlen(argv[1]) != strlen(argv[2])) {
 	fprintf(stderr, "Error: operands have different lengths");
 	exit(1);
   }
   int len = strlen(argv[1]);
   for (int i = 0; i < len; i++) {
     for (int j = 0; j < len; j++) {
-      if (argv[0][i] == argv[1][j]) {
+      if (argv[1][i] == argv[2][j]) {
 	fprintf(stderr, "Error: contains duplicate bytes");
 	exit(1);
       }
@@ -33,15 +32,15 @@ void checkInputs(int argc, char **argv) {
 
 
 int main(int argc, char **argv) {
-
+  
   checkInputs(argc, argv);
 
   char curr = getchar();
   while (curr != EOF) {
-    int len = strlen(argv[0]);
+    int len = strlen(argv[1]);
     for (int i = 0; i < len; i++) {
-      if (curr == argv[0][i]) {
-	curr = argv[1][i];
+      if (curr == argv[1][i]) {
+	curr = argv[2][i];
 	break;
       }
     }
--- as5/tr2u.c
+++ as5/tr2u.c
@@ -2,3 +2,56 @@
 getchar and putchar. The nbyte arguments to read and write
 should be 1, so that the program reads and writes single
 bytes at a time */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void checkInputs(int argc, char **argv) {
+  if (argc > 3) {
+    fprintf(stderr, "Error: too many operands.");
+    exit(1);
+  }
+  else if (argc < 3) {
+    fprintf(stderr, "Error: too few operands.");
+    exit(1);
+  }
+  else if (strlen(argv[1]) != strlen(argv[2])) {
+        fprintf(stderr, "Error: operands have different lengths");
+        exit(1);
+  }
+  int len = strlen(argv[1]);
+  for (int i = 0; i < len; i++) {
+    for (int j = 0; j < len; j++) {
+      if (argv[1][i] == argv[2][j]) {
+        fprintf(stderr, "Error: contains duplicate bytes");
+        exit(1);
+      }
+    }
+  }
+  return;
+}
+
+int main(int argc, char **argv) {
+
+  checkInputs(argc, argv);
+
+  
+  char in[1];
+  ssize_t fil = read(0, in, 1);
+  while (fil > 0) {
+    int len = strlen(argv[1]);
+    for (int i = 0; i < len; i++) {
+      if (in[0] == argv[1][i]) {
+        in[0] = argv[2][i];
+        break;
+      }
+    }
+    write(1, in, 1);
+    fil = read(0, in, 1);
+  }
+
+  exit(0);
+  
+}
